0bb3b67d9f2bf71b4a00c8a7e2d2f3a4f052e762
Jordi Parra Porcar <jordiparraporcar@gmail.com>
2022-04-19 23:22:48 +0900

    at this moment fetching every 5sec and storing all arrays in a big array

--- src/App.tsx
+++ src/App.tsx
@@ -12,38 +12,46 @@ function App() {
   };
 
   useEffect(() => {
-    fetchData()
-      .then((input) => {
-        const { updated_at, data } = input;
-        setTime(updated_at);
-        const entries = Object.entries(data);
-        const newArray = entries.map((el: any) => {
-          let arrayFirst;
-          let arraySecond;
-          [arrayFirst, arraySecond] = el;
-          const newObj: any = {};
-          newObj[`address`] = arrayFirst;
-          newObj["data"] = arraySecond;
-          return newObj;
-        });
-        setDataArray(newArray);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    const interval = setInterval(() => {
+      fetchData()
+        .then((input) => {
+          const { updated_at, data } = input;
+          setTime(updated_at);
+          const entries = Object.entries(data);
+          const newArray = entries.map((el: any) => {
+            let arrayFirst;
+            let arraySecond;
+            [arrayFirst, arraySecond] = el;
+            const newObj: any = {};
+            newObj[`address`] = arrayFirst;
+            newObj["data"] = arraySecond;
+            return newObj;
+          });
+          setDataArray((prevState: any) => [...prevState, newArray]);
+        })
+        .catch((err) => console.log(err));
+    }, 5000);
 
+    return () => clearInterval(interval);
+  }, []);
+  console.log(dataArray.length);
   return (
     <div className="App">
       <h1>timestamp: {time}</h1>
-      <ul>
-        {dataArray.map((el: any, i: any) => {
-          return (
-            <li key={i}>
-              <p>address: {el.address}</p>
-              <p>price: {el.data.price}</p>
-            </li>
-          );
-        })}
-      </ul>
+      {/* <ul>
+        {dataArray.length == 0 ? (
+          <p>Is Loading</p>
+        ) : (
+          dataArray.map((el: any, i: any) => {
+            return (
+              <li key={i}>
+                <p>address: {el.address}</p>
+                <p>price: {el.data.price}</p>
+              </li>
+            );
+          })
+        )}
+      </ul> */}
     </div>
   );
 }
